78f1609006210f0e5c3cd8b52f9dc2029ac4d8fe
Okysu <yby@ecanse.com>
2022-07-06 22:24:06 +0800

    del:优化屎山代码，删除了额外的滚动条。

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,24 +1,16 @@
 <template>
   <n-layout style="height: 100%">
     <n-layout position="absolute" style="top: 0px" has-sider>
-      <n-layout-sider
-        content-style="padding: 0px;"
-        :native-scrollbar="false"
-        collapse-mode="transform"
-        :collapsed-width="0"
-        :width="200"
-        bordered
-        show-trigger="bar"
-        :collapsed="collapsed"
-        position="static"
-        @collapse="collapsed = true"
-        @expand="collapsed = false"
-      >
+      <n-layout-sider content-style="padding: 0px;" :native-scrollbar="false" collapse-mode="transform"
+        :collapsed-width="0" :width="200" bordered show-trigger="bar" :collapsed="collapsed" position="static"
+        @collapse="collapsed = true" @expand="collapsed = false">
         <n-layout-header style="height: 44px; padding: 12px; text-align: center" bordered>Match Go</n-layout-header>
         <SideBar></SideBar>
       </n-layout-sider>
       <n-layout content-style="padding: 0px;" :native-scrollbar="false">
-        <RouterView></RouterView>
+        <n-scrollbar>
+          <RouterView></RouterView>
+        </n-scrollbar>
       </n-layout>
     </n-layout>
   </n-layout>
@@ -32,6 +24,7 @@ import {
   NLayout,
   NLayoutHeader,
   NLayoutSider,
+  NScrollbar
 } from "naive-ui";
 // 引入自定义组件
 import SideBar from "@/components/layout/SideBar.vue";
