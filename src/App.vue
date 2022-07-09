67a40f00b7150731c974e9409a29f6ac19724f34
Okysu <yby@ecanse.com>
2022-07-09 22:00:55 +0800

    add:为额外路由页面添加滚动条

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { RouterView } from "vue-router";
-import { NNotificationProvider } from "naive-ui";
+import { NNotificationProvider, NScrollbar } from "naive-ui";
 //引入通知组件
 import NotificationAPI from "@/components/main/NotificationAPI.vue";
 const handleShowNotification = (type: string, title: string, message: string, duration: number = 1000) => {
@@ -49,7 +49,10 @@ onMounted(() => {
   <n-notification-provider placement="top">
     <NotificationAPI />
   </n-notification-provider>
-  <RouterView />
+  <n-scrollbar>
+    <RouterView />
+  </n-scrollbar>
+
 </template>
 <style>
 #root,
