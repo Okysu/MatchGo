b9d9c50bdcc47a5b34bb1531acf103d3c230de90
Okysu <yby@ecanse.com>
2022-07-09 22:00:30 +0800

    add:新增highlight.js的代码高亮支持

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { createPinia } from "pinia";
 import piniaPluginPersist from 'pinia-plugin-persist'
 import App from "./App.vue";
 import router from "./router";
+import hljs from 'highlight.js' //导入代码高亮文件
+import 'highlight.js/styles/monokai-sublime.css'  //导入代码高亮样式
 
 const store = createPinia()
 store.use(piniaPluginPersist)
 
 const app = createApp(App);
+app.directive('highlight', function (el) {
+    const blocks = el.querySelectorAll('pre code');
+    blocks.forEach((block: any) => {
+        hljs.highlightBlock(block)
+    })
+})
 
 app.use(store);
 app.use(router);
